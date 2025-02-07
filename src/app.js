d68d1547138acfb8e71ce296ff3c18734462a676
giusmili <giusmili67@gmail.com>
2025-02-07 10:28:55 +0100

    objet

--- src/app.js
+++ src/app.js
@@ -1,24 +1,38 @@
 document.addEventListener("DOMContentLoaded",(e)=>{
         e.preventDefault()
 
-        const inputNom = document.querySelectorAll('input[type="text"]')[0]
-        const inputPrenom = document.getElementsByTagName("input")[1]
-        const inputNumber = document.getElementsByTagName("input")[2]
+ 
         const btn_submit = document.getElementsByTagName('input')[3]
-        const element = document.querySelector(".warning")
-        console.log(element)
+       
+
+
+        const User = {
+
+            inputNom: document.querySelectorAll('input[type="text"]')[0],
+            inputPrenom: document.getElementsByTagName("input")[1],
+            inputNumber: document.getElementsByTagName("input")[2],
+            element: document.querySelector(".warning"),
+
+            getUser(){
+                if(this.inputNom.value && this.inputPrenom.value && this.inputNumber.value){
+                    this.element.classList.add("warning-success")
+                    this.element.classList.remove("warning-alert")
+                    this.element.innerText = `Bonjour ${this.inputNom.value} ${this.inputPrenom.value } ${this.inputNumber.value}` 
+                }
+                else{
+                    this.element.classList.remove("warning-success")
+                    this.element.classList.add("warning-alert")
+                    this.element.innerHTML =  "Remplir les champs"
+                }
+            }
+
+        }
+   
 
         btn_submit.addEventListener("click",(e)=>{
             e.preventDefault()
-            if(inputNom.value && inputPrenom.value && inputNumber.value){
-                
-                element.innerText = `Bonjour ${inputNom.value} ${inputPrenom.value } ${inputNumber.value}` 
-            }
-            else{
-                element.classList.add("warning-alert")
-                element.innerHTML =  "Remplir les champs"
-            }
-            
+           
+            User.getUser()
             
         })
             
