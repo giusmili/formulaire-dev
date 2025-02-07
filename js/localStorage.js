ab875430e13d1e48f95bcb60deabda57b7c023db
giusmili <giusmili67@gmail.com>
2025-02-07 12:27:42 +0100

    js

---
+++ js/localStorage.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", e => {
+        e.preventDefault();
+        const parent = document.querySelector("section");
+        const title = document.createElement("h2");
+        const stockUser = localStorage.getItem("user");
+        const eventButton = document.querySelector("section button");
+        const stockSession = sessionStorage.getItem("app");
+        parent.prepend(title);
+        title.innerHTML += `<span aria-hidden="true">👍</span>Bonjour ${stockUser} ${stockSession}`;
+
+        eventButton.addEventListener("click", () => {
+                document.location.href = "index.html";
+                sessionStorage.clear();
+                localStorage.clear();
+        });
+});
